67c462bc6e39719bf656e63ef777e38e7816ae46
Polimpo4 <114686525+JesseMotaMendonca@users.noreply.github.com>
2023-07-14 21:53:57 +0000

    Update segredo.php

--- segredo.php
+++ segredo.php
@@ -4,6 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 
 header("Access-Control-Allow-Headers: X-Requested-With");
-$segredo = '1234567';
+
+define("segredo","1234567");
 
 ?>
